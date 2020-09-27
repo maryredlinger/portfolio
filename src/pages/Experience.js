7b57757dfae90b4ab5baeb0e0a2b3b61de5dbfe8
maryredlinger <mary@redlinger.net>
2020-09-27 16:51:37 -0700

    more work

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -81,32 +81,62 @@ class Experience extends React.Component{
                 </div>
                 <hr />
                 <div className="other experience-flex">
+
+                <div id="proj8">
+                        <div className="exp">
+                            <a onClick={this.openPopup.bind(this)} data-id ="athanasius" title="Click to Learn More"  >
+                                <h1>Athanasius Academia</h1>
+                                <img src='athanasius.png'></img>
+                            </a>
+                        </div>
+                    </div>
+
+                <div id="proj7">
+                        <div className="exp">
+                            <a onClick={this.openPopup.bind(this)} data-id ="sosl" title="Click to Learn More"  >
+                                <h1>Survivors of Suicide Loss</h1>
+                                <img src='sosl.png'></img>
+                            </a>
+                        </div>
+                    </div>
+
+
+                <div id="proj6">
+                        <div className="exp">
+                            <a onClick={this.openPopup.bind(this)} data-id ="ride-for-life" title="Click to Learn More"  >
+                                <h1>Ride For Life</h1>
+                                <img src='ride-for-life.png'></img>
+                            </a>
+                        </div>
+                    </div>
+
                     <div id="proj5">
                         <div className="exp">
+                        <a onClick={this.openPopup.bind(this)} data-id ="walk-in-hope" title="Click to Learn More"  >
                             <h1>Walk In Remembrance</h1>
                             <img src='walk_in_hope.png'></img>
-                            <a onClick={this.openPopup.bind(this)} data-id ="walk-in-hope"  ><h1>HELLO</h1></a>
+                            </a>
 
                             </div>
                     </div>
                     <div id="proj4">
                         <div className="exp">
-                            <h1>Stellar High Five</h1>
+                        <a onClick={this.openPopup.bind(this)} data-id ="stellar" title="Click to Learn More"  ><h1>Stellar High Five</h1>
                             <img src='shf.png'></img>
-                            <a onClick={this.openPopup.bind(this)} data-id ="stellar"  ><h1>HELLO</h1></a>
+                            </a>
 
-                        </div>
-                    </div>
-                    <div id="proj3">
-                        <div className="exp">
-                            <h1>Tic Tac Toe</h1>
-                            <img src='tic_tac.png'></img>
                         </div>
                     </div>
                     <div id="proj2">
                         <div className="exp">
                             <h1>Hidden Gems</h1>
                             <img src='hidden_gems.png'></img>
+                        </div>
+                    </div>
+                    <div id="proj3">
+                        <div className="exp">
+                            <h1>Tic Tac Toe</h1>
+                            <img src='tic_tac.png'></img>
                         </div>
                     </div>
                     <div id="proj1">
@@ -117,25 +147,48 @@ class Experience extends React.Component{
                     </div>
                 </div>
             </div>
-
-                <div className="boxPopup" id="box-walk-in-hope">
+                <div className="popup">
+                <div className="boxPopup " id="box-walk-in-hope">
                 <a onClick={this.closePopup.bind(this)}><h1>&times;</h1></a>
                     <h3>I was tasked to complete a site used to host all information and media for Survivors of Suicide Loss (SOSL)'s event Walk in Rememberance with Hope. I used the CMS RallyBound to add to the exsisting site and help maintain this site until the end of the fundraising event in September 2020. I had to create blog posts, tickets for registration and sponsorship, as well as keeping up with all COVID changes</h3>
+                    <a><img  title="View GitHub Repo" alt="GitHub Icon" src="git.png"></img></a>
+                    <a><img  title="View Website" alt="website Icon" src="www.png"></img></a>
+
                 </div>
                 <div className="boxPopup" id="box-stellar">
                 <a onClick={this.closePopup.bind(this)}><h1>&times;</h1></a>
                     <h3>Stellar High Five is a project I was a part of during my internship with Zeal - a development consultancy comapny. I was part of a team of 4, where I had the role of updating and brining back to life a 5 year old web application, Stellar High Five, featuring a new React front end.</h3>
+                    <a><img title="View GitHub Repo" alt="GitHub Icon" src="git.png"></img></a>
+                    <a><img  title="View Website" alt="website Icon" src="www.png"></img></a>
+
+
                 </div>
 
+                <div className="boxPopup" id="box-ride-for-life">
+                <a onClick={this.closePopup.bind(this)}><h1>&times;</h1></a>
+                    <h3>I was tasked to complete a site used to host all information and media for Survivors of Suicide Loss (SOSL)'s event Ride For Life. I used the CMS RallyBound to add to the exsisting site and help maintain this site until the end of the fundraising event in October 2020.</h3>
+                    <a><img title="View GitHub Repo" alt="GitHub Icon" src="git.png"></img></a>
+                    <a><img  title="View Website" alt="website Icon" src="www.png"></img></a>
+                </div>
+                <div className="boxPopup" id="box-sosl">
+                <a onClick={this.closePopup.bind(this)}><h1>&times;</h1></a>
+                    <h3>I was tasked to update SOSL's main website using the CMS WordPress. I am on contract until December to make the site more user and mobile friendly. I have been working on this site since the end of July 2020 and am in the process of sketching a new front page and layout for the site using WordPress and have just completed simplifying the Bookstore section under Resources.</h3>
+                    <a><img title="View GitHub Repo" alt="GitHub Icon" src="git.png"></img></a>
+                    <a><img  title="View Website" alt="website Icon" src="www.png"></img></a>
+                </div>
+
+                <div className="boxPopup" id="box-athanasius">
+                <a onClick={this.closePopup.bind(this)}><h1>&times;</h1></a>
+                    <h3>I made a site for a retired teacher.</h3>
+                    <a><img title="View GitHub Repo" alt="GitHub Icon" src="git.png"></img></a>
+                    <a><img  title="View Website" alt="website Icon" src="www.png"></img></a>
+                </div>
+
+                
                 <div className="boxPopup" id="box-">
                     <h1>hi</h1>
                 </div>
-
-                <div className="boxPopup" id="box-">
-                    <h1>hi</h1>
-                </div>
-
-\                
+                </div>        
             </div>
         )
     }
