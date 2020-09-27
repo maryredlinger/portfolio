a3470a9e1dc5ae895c340d449e302f967fabfb39
maryredlinger <mary@redlinger.net>
2020-09-27 15:56:22 -0700

    adds pop up

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,7 +21,6 @@ const About = () => {
                     <h4 className="that-font">Rock Climber / Corgi Enthusiast</h4>
                 </div>
             </div>
-                <p>My name is Mary Redlinger and I am a new developer in the San Diego area looking to break into the tech industry to put my skills to work. I am a graduate from LEARN academy, an innovative full stack bootcamp based in downtown San Diego, where I became proficient in Full Stack Development and Object Oriented Programming. I would love to work for a company that plays a role in creating a better future for current and future generations and especially seeking out a company that is accessible.</p>
         </div>
     )
 }
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -9,6 +9,12 @@ import { render } from '@testing-library/react'
 
 
 class Experience extends React.Component{
+    constructor(){
+        super()
+            this.state = {
+                id : '',
+            }
+    }
     javaScript(){
         var MyDiv1 = document.getElementById('proj1').innerHTML;
         var MyDiv2 = document.getElementById('proj2').innerHTML;
@@ -39,9 +45,23 @@ class Experience extends React.Component{
         div.innerHTML += MyDiv4
     }
 
+    openPopup(e){
+        var id = e.currentTarget.dataset.id
+        var box = document.getElementById(`box-${id}`)
+        this.setState({ id : id})
+        console.log("her", e.currentTarget.dataset.id)
+        box.style.display = "block";
+    }
+    
+    closePopup(){
+        var id = this.state.id
+        var box = document.getElementById(`box-${id}`)
+        box.style.display = "none";
+    }
     render(){
+        console.log(this.state.id)
         return(
-            <div>
+            <div onClick={this.closePopup.bind(this)}>
                 <Nav />
             <div className="experience">
                 <div className="experience-container nav">
@@ -55,7 +75,7 @@ class Experience extends React.Component{
                     <a onClick={this.javaScript}> <img src='postgres-icon.png'></img></a>
                     <a onClick={this.javaScript}> <img src='wordpress-icon.png'></img></a>
                     <a onClick={this.javaScript}><img src='css-icon.png'></img></a>
-                    <a onClick={this.javaScript}><img src='bootstrap-icon.png'></img></a>
+                    <a onClick={this.openPopup}><img src='bootstrap-icon.png'></img></a>
                 </div>
                 <div className="other show-flex" id="showCurrent">
                 </div>
@@ -64,35 +84,58 @@ class Experience extends React.Component{
                     <div id="proj5">
                         <div className="exp">
                             <h1>Walk In Remembrance</h1>
-                            <img src='css-icon.png'></img>
-                        </div>
+                            <img src='walk_in_hope.png'></img>
+                            <a onClick={this.openPopup.bind(this)} data-id ="walk-in-hope"  ><h1>HELLO</h1></a>
+
+                            </div>
                     </div>
                     <div id="proj4">
                         <div className="exp">
                             <h1>Stellar High Five</h1>
-                            <img src='ruby-icon.png'></img>
+                            <img src='shf.png'></img>
+                            <a onClick={this.openPopup.bind(this)} data-id ="stellar"  ><h1>HELLO</h1></a>
+
                         </div>
                     </div>
                     <div id="proj3">
                         <div className="exp">
-                            <h1>Tik Tak Toe</h1>
-                            <img src='react-icon.png'></img>
+                            <h1>Tic Tac Toe</h1>
+                            <img src='tic_tac.png'></img>
                         </div>
                     </div>
                     <div id="proj2">
                         <div className="exp">
                             <h1>Hidden Gems</h1>
-                            <img src='bootstrap-icon.png'></img>
+                            <img src='hidden_gems.png'></img>
                         </div>
                     </div>
                     <div id="proj1">
                         <div className="exp">
                             <h1>Divine Mercy Conference</h1>
-                            <img src='angular-icon.png'></img>
+                            <img src='divine.png'></img>
                         </div>
                     </div>
                 </div>
             </div>
+
+                <div className="boxPopup" id="box-walk-in-hope">
+                <a onClick={this.closePopup.bind(this)}><h1>&times;</h1></a>
+                    <h3>I was tasked to complete a site used to host all information and media for Survivors of Suicide Loss (SOSL)'s event Walk in Rememberance with Hope. I used the CMS RallyBound to add to the exsisting site and help maintain this site until the end of the fundraising event in September 2020. I had to create blog posts, tickets for registration and sponsorship, as well as keeping up with all COVID changes</h3>
+                </div>
+                <div className="boxPopup" id="box-stellar">
+                <a onClick={this.closePopup.bind(this)}><h1>&times;</h1></a>
+                    <h3>Stellar High Five is a project I was a part of during my internship with Zeal - a development consultancy comapny. I was part of a team of 4, where I had the role of updating and brining back to life a 5 year old web application, Stellar High Five, featuring a new React front end.</h3>
+                </div>
+
+                <div className="boxPopup" id="box-">
+                    <h1>hi</h1>
+                </div>
+
+                <div className="boxPopup" id="box-">
+                    <h1>hi</h1>
+                </div>
+
+\                
             </div>
         )
     }
